210f0045bb394a46aff5621daf7686d3cf621c51
Ashlyn Knox <ashlyn@lilyx.io>
2023-11-02 11:17:49 -0600

    component application comments
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

---
+++ src/lib/components/MessageBox.svelte
@@ -0,0 +1,6 @@
+<div class="bg-blue-100 rounded-sm p-4 max-w-sm">
+	<h3 class="text-xl pb-2">The Message</h3>
+	<p class="text-gray-500">
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, laborum.
+	</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	// import the MessageBox component
+
 	// render html with @html in the rendered variable { ... }
 	const dailyContent = '<span class="text-blue-500">Course Content</span>';
 	const description =
@@ -8,12 +10,7 @@
 <h1 class="text-5xl">SAIT Svelte {@html dailyContent}</h1>
 <p>{description}</p>
 
-<div class="bg-blue-100 rounded-sm p-4 max-w-sm">
-	<h3 class="text-xl pb-2">The Message</h3>
-	<p class="text-gray-500">
-		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, laborum.
-	</p>
-</div>
+<!-- Render the MessageBox Component -->
 
 <p>
 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum neque deserunt aut consequatur
