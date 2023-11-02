2768c4191e1678888eab0024986d41589e21d33d
Ashlyn Knox <ashlyn@lilyx.io>
2023-11-02 11:26:44 -0600

    component placeholder init
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

--- src/lib/components/MessageBox.svelte
+++ src/lib/components/MessageBox.svelte
@@ -1,6 +1,11 @@
-<div class="bg-blue-100 rounded-sm p-4 max-w-sm">
-	<h3 class="text-xl pb-2">The Message</h3>
+<script>
+	export let title = 'placeholder title';
+	export let description = 'placeholder description';
+</script>
+
+<div class="bg-blue-100 rounded-sm p-4 my-2 max-w-sm">
+	<h3 class="text-xl pb-2">{title}</h3>
 	<p class="text-gray-500">
-		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, laborum.
+		{description}
 	</p>
 </div>
