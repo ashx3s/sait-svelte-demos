af4b9f9562ee250434c299b4ddaa3be246b53c5a
Ashlyn Knox <ashlyn@lilyx.io>
2023-11-03 10:07:55 -0600

    component slot example
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

---
+++ src/lib/components/ListItem.svelte
@@ -0,0 +1,1 @@
+<li class="my-2 text-2xl bg-red-100 rounded-xl max-w-xs px-4 py-2"><slot /></li>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,32 @@
 <script>
+	import ListItem from '../../lib/components/ListItem.svelte';
+	let listItems = ['Dog', 'Cat', 'Crow ', 'Ferret'];
+	let secondList = [
+		{ name: 'Alfred', species: 'Dog', href: '/' },
+		{ name: 'Elvira', species: 'Cat', href: '/' },
+		{ name: 'Sally', species: 'Crow', href: '/' },
+		{ name: 'Gus', species: 'Ferret', href: '/' }
+	];
 </script>
 
-<header>
+<header class="my-4 text-center text-5xl">
 	<h1>About Page</h1>
 </header>
+
+<section>
+	<h2 class="text-3xl">Our Example List</h2>
+	<ul class="list-disc ml-8">
+		{#each listItems as item}
+			<ListItem>{item}</ListItem>
+		{/each}
+	</ul>
+</section>
+
+<section>
+	<h2 class="text-3xl">Animals</h2>
+	<ul>
+		{#each secondList as { href, name, species }}
+			<ListItem><a {href}>{name} is a {species}</a></ListItem>
+		{/each}
+	</ul>
+</section>
