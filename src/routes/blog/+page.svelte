936cb8bb6ddfac5784cffdebec189b6b4871b2bf
Ashlyn Knox <ashlyn@lilyx.io>
2023-11-06 11:10:39 -0700

    minimal conditional example name check
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -6,6 +6,29 @@
 		description: 'This is a blog about tech and mystery machines.',
 		bgColor: 'bg-blue-500'
 	};
+	// TODO: address capitalization on input (strings should be able to be lowercase but always return proper capitalized)
+	const name = 'ash';
 </script>
 
 <PageHeader {...headerInfo} />
+
+{#if name === 'Godfrey'}
+	<p>Stop trying to hack my system Godfrey</p>
+{:else if name === 'Ash'}
+	<p>Good morning {name}, even in the afternoon</p>
+{:else}
+	<p>Hello {name}</p>
+{/if}
+
+<!-- 
+if (input === "Ash") {
+    console.log("hello")
+    return "hello";
+  } else if (input === "Godfrey") {
+    console.log("Stop trying to log into my syste Godfrey");
+    return "stop hacking Godfrey";
+  } else {
+    console.log("signed out");
+    return "signed out";
+  }
+ -->
